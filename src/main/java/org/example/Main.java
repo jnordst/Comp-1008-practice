6339a01d82a4765dd51209bfb759fd092f0d73cd
Jacob N <levolpehh@hotmail.com>
2023-02-02 11:22:44 -0500

    created 5 instances of VideoGames

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,16 @@ public class Main {
     public static void main(String[] args)
     {
         ShoppingCart cart = new ShoppingCart();
+
         VideoGame fireEmblem = new VideoGame("Fire Emblem: Engage", 79.99);
         VideoGame finalFantasy = new VideoGame("Final Fantasy XIV", 24.99);
+        VideoGame octopathTraveler = new VideoGame("Octopath Traveler", 64.99);
+        VideoGame triangleStrategy = new VideoGame("Triangle Strategy", 79.99);
+        VideoGame xenobladeChronicles = new VideoGame("Xenoblade Chronicles 3", 79.99);
+
         cart.AddToCart(fireEmblem);
         cart.AddToCart(finalFantasy);
+
 
 
         for (VideoGame game : cart.ItemsInCart()) {
